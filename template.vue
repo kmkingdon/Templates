8039ae199f9a6c272c1a6ae6a16f3a3e1d20e3a6
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-04-18 13:17:01 -0600

    adding vue component template

---
+++ template.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="">
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: '',
+  components: {
+  },
+  data() {
+    return {
+    };
+  },
+  computed: mapGetters([
+  ]),
+  methods: mapActions([
+      'getLessonPlans',
+      'getComponents',
+      'getGoals',
+      'postGoal',
+      'saveFinalReflection',
+  ]),
+  mounted(){
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
